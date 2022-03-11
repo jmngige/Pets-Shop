354bd94f9ae19f4ff97189d45148402336580eb9
jmngige <jamesmngige@gmail.com>
2022-03-11 23:42:20 +0300

    App permissions commit

--- app/src/main/java/com/starsolns/pets/views/ui/MainActivity.kt
+++ app/src/main/java/com/starsolns/pets/views/ui/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.starsolns.pets.views.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.starsolns.pets.R
-import com.starsolns.pets.databinding.FragmentListBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -20,10 +21,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHost = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHost.findNavController()
 
         setupActionBarWithNavController(navController)
+
+
+        requestPermissions()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -39,7 +44,49 @@ class MainActivity : AppCompatActivity() {
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
-    private fun loadPermissions(){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 101 && grantResults.isNotEmpty()){
 
+        }
     }
+
+    private fun hasStoragePerm() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasFineLocationPerm() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasCoarseLocationPerm() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestPermissions(){
+        val requestedPerms = mutableListOf<String>()
+
+        if(!hasStoragePerm()){
+            requestedPerms.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if(!hasFineLocationPerm()){
+            requestedPerms.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        if(!hasCoarseLocationPerm()){
+            requestedPerms.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if (requestedPerms.isNotEmpty()){
+            ActivityCompat.requestPermissions(this, requestedPerms.toTypedArray(), 101)
+        }
+    }
+
 }
